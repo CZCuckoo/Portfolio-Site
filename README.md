5c17f41d9e8cad079cd5c88b11a7fd2c3b64267c
unknown <Clint.Johnson.07@gmail.com>
2022-09-20 13:08:46 -0400

    Made sure files were structured correctly

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-# Portfolio-Site
-A portfolio site made to demonstrate my HTML skills, as well as hosting various projects I've built.
-
-This is the first build of a portfolio site made for Udacity.com.
+# Movie Trailer Website
+This is a movie trailer Website built to learn and practice Python.
 
 ## Instructions
-This project contains no instructions beyond opening the index.html in your favorite browser. It is built to be responsive, so it will work on multiple machines and widths. In addition, I've used normalize.css to help standardize what this site will look like across browsers. This is the only code not written entirely by me, and its information is as follows.
+This project was built using Python 2.7.13, and requires at least this version to run. Three files work together to make this project work, and must be downloaded and placed in the same directory. Run entertainment_center.py to use this project. Should you want to add your own movies to this site, modify entertainment_center.py in order to create additional instances. Be sure to include all new movies in the array "movies".
 
-normalize.css v7.0.0 | MIT License | github.com/necolas/normalize.css
+## File Breakdown
+This project consists of media.py, fresh_tomatoes.py, and entertainment_center.py. Media.py contains a class "Movie", which is used to create instances of whichever movies you want to include in your site. Fresh_tomatoes.py contains HTML code, and two functions to dynamically create movie titles, and render each clickable movie poster.  Entertainment_center.py creates instances of the class Movie, containing all of the relevant information, catalogues all instances in an array, and opens fresh_tomatoes.py.
---
+++ entertainment_center.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+"""This file is the main file to be run for our fresh tomatoes site.
+It brings in the class Movie from media.py, and runs fresh_tomatoes.py
+to get the html necessary to run the project. It creates instances of
+the class Movie, and an array used as an argument in fresh_tomatoes"""
+
+import media
+import fresh_tomatoes
+
+# This is a series of instances of the class Movie, taking the name, summary,
+# movie poster, and trailer link as arguments.
+
+fury_road = media.Movie("Mad Max: Fury Road",
+                        "In a post apocalyptic world, two people struggle to "
+                        "escape a warlord",
+                        "https://upload.wikimedia.org/wikipedia/en/6/6e/Mad_Max_Fury_Road.jpg",  # NOQA
+                        "https://youtu.be/hEJnMQG9ev8")
+
+blade_runner = media.Movie("Blade Runner",
+                           "A bounty hunter seeks out robotic 'replicants' "
+                           "learns more about his prey, "
+                           "and questions his job.",
+                           "https://upload.wikimedia.org/wikipedia/en/5/53/Blade_Runner_poster.jpg",  # NOQA
+                           "https://youtu.be/eogpIG53Cis")
+
+force_awakens = media.Movie("Star Wars: The Force Awakens",
+                            "A young woman discovers she is "
+                            "strong in the force.",
+                            "https://upload.wikimedia.org/wikipedia/en/a/a2/Star_Wars_The_Force_Awakens_Theatrical_Poster.jpg",  # NOQA
+                            "https://youtu.be/sGbxmsDFVnE")
+
+aliens = media.Movie("Aliens",
+                     "A team of marines discover investigate a planet and get "
+                     "more than they anticipated",
+                     "https://upload.wikimedia.org/wikipedia/en/f/fb/Aliens_poster.jpg",  # NOQA
+                     "https://youtu.be/W857ys3BlRI")
+
+princess_bride = media.Movie("The Princess Bride",
+                             "A young child, home from school, bonds with his "
+                             "grandfather as he is read a story",
+                             "https://upload.wikimedia.org/wikipedia/en/d/db/Princess_bride.jpg",  # NOQA
+                             "https://youtu.be/VYgcrny2hRs")
+
+arrival = media.Movie("The Arrival",
+                      "A translator must quickly learn an alien language to "
+                      "stop violence.",
+                      "https://upload.wikimedia.org/wikipedia/en/d/df/Arrival%2C_Movie_Poster.jpg",  # NOQA
+                      "https://youtu.be/tFMo3UJ4B4g")
+
+movies = [fury_road, blade_runner, force_awakens, aliens, princess_bride,
+          arrival]
+
+# the array movies includes all of the instances of Movie class, which are
+# called within fresh_tomatoes.py as arguments in create_movie_titles_content
+# and open_movies_page
+
+fresh_tomatoes.open_movies_page(movies)
